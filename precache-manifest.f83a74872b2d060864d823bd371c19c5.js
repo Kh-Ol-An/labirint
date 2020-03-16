0f43a42f0ccc48882056de20f93bbaab73b174b2
Kh-Ol-An <olegkhristenko@gmail.com>
2020-03-16 20:29:01 +0200

    Updates

---
+++ precache-manifest.f83a74872b2d060864d823bd371c19c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996ed8b4bedeb34e5f67fadd19e96990",
+    "url": "/labirint/index.html"
+  },
+  {
+    "revision": "28287868780d6f71c279",
+    "url": "/labirint/static/css/main.d990ed8f.chunk.css"
+  },
+  {
+    "revision": "73f266192d25e6bad4ff",
+    "url": "/labirint/static/js/2.59a8b21e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/labirint/static/js/2.59a8b21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28287868780d6f71c279",
+    "url": "/labirint/static/js/main.51377dd4.chunk.js"
+  },
+  {
+    "revision": "82b176dde1ce2a2128af",
+    "url": "/labirint/static/js/runtime-main.d7e6c729.js"
+  }
+]);
